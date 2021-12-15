6ffc80b08d2448e7747e966f41d723cfba7bc48f
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-12-15 19:07:32 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    target: "web",
-    devtool: "inline-source-map",
-    output: {
-        path: path.resolve(__dirname, "dist"),
+  target: 'web',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, '/dist'),
+    open: true,
+    compress: true,
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
     },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: path.resolve(__dirname, "/dist"),
-        open: true,
-        compress: true,
+    {
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+      }],
     },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: "html-loader",
-                }, ],
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-        ],
+    {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader'],
     },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css",
-        }),
-        new webpack.HotModuleReplacementPlugin(),
     ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
